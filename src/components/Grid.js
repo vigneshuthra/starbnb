8fa37aa9afa62cfa129fea367c22f359b788651a
vigneshuthra <vigneshuthra@gmail.com>
2023-07-26 18:41:33 +0100

    added layout for landing page

---
+++ src/components/Grid.js
@@ -0,0 +1,16 @@
+import React from "react";
+import gridimg from "../images/photo-grid.png";
+
+
+export default function Grid() {
+  return (
+    <section className="gridcontainer">
+      <img src={gridimg} className="grid--img"/>
+      <h1 className="grid--header">Experiences</h1>
+      <p className="grid--text">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "../images/stars-png-614.png";
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <img className="nav-logo" src={logo} alt="StarBnb" /> 
+    </nav>
+  );
+}

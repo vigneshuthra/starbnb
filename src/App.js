624fca5e1ef4b3fc56f1ddb74f05415564527092
vigneshuthra <vigneshuthra@gmail.com>
2023-07-27 16:11:22 +0100

    added card layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import "./style.css";
 import Grid from "./components/Grid";
 import Card from "./components/Card";
-     // <Grid/>
+import cardimg from "./images/katie-zaferes.png";
+
+// <Grid/>
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Card/>
+      <Card
+        img={cardimg}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,18 @@
 import React from "react";
-import cardimg from "../images/katie-zaferes.png";
 import star from "../images/star.png";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
-      <img className="card--img" src={cardimg} />
+      <img className="card--img" src={props.img} />
       <div className="card--stats">
         <img src={star} className="card--star" />
-        <span >5.0</span>
-        <span className="gray">(6) . </span>
-        <span className="gray">USA</span>
+        <span >{props.rating}</span>
+        <span className="gray">({props.reviewCount}) . </span>
+        <span className="gray">{props.country}</span>
       </div>
-      <p>Life Lessons with Katie Zaferes</p>
-      <p><span className="card--price">From $136 </span>/ person</p>
+      <p>{props.title}</p>
+      <p><span className="card--price">From ${props.price} </span>/ person</p>
     </div>
   );
 }
